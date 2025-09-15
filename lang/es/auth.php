621f46274db42fe608146f4de190ae5b1c70b7dc
Nicolas <gamboanicolas58@gmail.com>
2025-09-15 17:25:14 -0600

    chore: admin route added

---
+++ lang/es/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+    'login_button' => 'Iniciar sesión',
+    'register_button' => 'Registrarse',
+];
