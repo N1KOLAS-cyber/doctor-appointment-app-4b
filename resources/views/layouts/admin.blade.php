6f20479137e2ebdfe404d85f60b58a6d2911ee07
Nicolas <gamboanicolas58@gmail.com>
2025-10-15 19:54:47 -0600

    feat(layout): add breadcrumb I component and integrate into admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,5 +1,29 @@
-<!DOCTYPE html>
+@props(['breadcrumbs' => []])
+
+    <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name', 'Laravel') }}</title>
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+<!-- Scripts -->
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+<script src="https://kit.fontawesome.com/9161014f5f.js" crossorigin="anonymous"></script>
+
+<!-- Styles -->
+<head>
+    ...
+    <wireui:scripts />
+    <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+@livewireStyles
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,37 +31,25 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="https://kit.fontawesome.com/08f05281c6.js" crossorigin="anonymous"></script>
-
-
-    <!-- Styles -->
     @livewireStyles
 </head>
 <body class="font-sans antialiased bg-gray-500">
-
 @include('layouts.includes.admin.navigation')
-
 @include('layouts.includes.admin.sidebar')
-        <div class="p-4 sm:ml-64">
-            <!--margin top 14-->
-            <div class="mt-14">
-                {{$slot}}
-            </div>
 
-        </div>
+<div class="p-4 sm:ml-64">
+    {{-- añadiendo margen superior --}}
+    <div class="mt-14 flex items-center justify-between w-full">
+        @include('layouts.includes.admin.breadcrumb')
+    </div>
+    {{ $slot }}
+</div>
 
 @stack('modals')
 
 @livewireScripts
 <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 
-{{--wireui--}}
-<wireui:scripts />
 </body>
 </html>
